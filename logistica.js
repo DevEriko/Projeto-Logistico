826ba981c66fdad8492509a7e1ec65e21973b81c
Ériko Fernando <eriko.bgoncalves@gmail.com>
2025-01-07 10:29:50 -0300

    Função / Parâmetros / swith... Criando a função para calcular o custo de envio de um item.

---
+++ logistica.js
@@ -0,0 +1,18 @@
+// Função para calcular o custo de envio.
+function calcularCustoEnvio(peso, distancia, tipoTransporte) {
+    let custoPorKm;
+    switch (tipoTransporte.toLowerCase()) {
+        case "terrestre":
+            custoPorKm = 0.05; // Custo por km em transporte terrestre
+            break;
+        case "aéreo":
+            custoPorKm = 0.1; // Custo por km em transporte aéreo
+            break;
+        case "marítimo":
+            custoPorKm = 0.03; // Custo por km em transporte marítimo
+            break;
+        default:
+            console.log("Tipo de transporte inválido.");
+            return null;
+    }
+}
